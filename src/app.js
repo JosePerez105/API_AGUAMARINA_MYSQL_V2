92783b1b0e813473ac1df9104f5b77990681eaea
JosePerez <joseleopepe1033@gmail.com>
2024-09-04 01:07:01 -0500

    Deploy 1.10

--- src/app.js
+++ src/app.js
@@ -2,23 +2,23 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import rolesRoutes from './routes/1_Roles.routes.js';
-import permissionsRoutes from './routes/2_permissions.routes.js';
-import citiesRoutes from './routes/4_Cities.routes.js';
-import addressesRoutes from './routes/5_Addresses.routes.js';
-import categoriesRoutes from './routes/6_Categories.routes.js';
-import productsRoutes from './routes/7_Products.routes.js';
-import imagesRoutes from './routes/8_Images.routes.js';
-import statusRoutes from './routes/9_Status.routes.js';
-import reservationsRoutes from './routes/10_Reservation.routes.js';
-import reservationDetailsRoutes from './routes/11_ReservationDetails.routes.js';
-import rentsRoutes from './routes/12_Rents.routes.js';
-import paymentRegistersRoutes from './routes/13_PaymentRegisters.routes.js';
-import checkListsRoutes from './routes/14_CheckLists.routes.js';
-import checkListItemsRoutes from './routes/15_CheckListItems.routes.js';
-import purchasesRoutes from './routes/16_Purchases.routes.js';
-import purchaseDetailsRoutes from './routes/17_PurchaseDetails.routes.js';
-import verificationCodesRoutes from './routes/19_VerificationCodes.routes.js'
+import rolesRoutes from '../routes/1_Roles.routes.js';
+import permissionsRoutes from '../routes/2_permissions.routes.js';
+import citiesRoutes from '../routes/4_Cities.routes.js';
+import addressesRoutes from '../routes/5_Addresses.routes.js';
+import categoriesRoutes from '../routes/6_Categories.routes.js';
+import productsRoutes from '../routes/7_Products.routes.js';
+import imagesRoutes from '../routes/8_Images.routes.js';
+import statusRoutes from '../routes/9_Status.routes.js';
+import reservationsRoutes from '../routes/10_Reservation.routes.js';
+import reservationDetailsRoutes from '../routes/11_ReservationDetails.routes.js';
+import rentsRoutes from '../routes/12_Rents.routes.js';
+import paymentRegistersRoutes from '../routes/13_PaymentRegisters.routes.js';
+import checkListsRoutes from '../routes/14_CheckLists.routes.js';
+import checkListItemsRoutes from '../routes/15_CheckListItems.routes.js';
+import purchasesRoutes from '../routes/16_Purchases.routes.js';
+import purchaseDetailsRoutes from '../routes/17_PurchaseDetails.routes.js';
+import verificationCodesRoutes from '../routes/19_VerificationCodes.routes.js'
 
 import './db/initialization.js';
 
